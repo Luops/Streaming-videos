e07ab52a520a71656111a686e11c2358435470a5
fabricio rios <you@example.com>
2022-10-03 11:55:05 -0300

    Iniciado o projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { 
+  BrowserRouter, 
+  Routes, 
+  Route,
+  Router, 
+  Navigate
+} from 'react-router-dom'
+
+//Paginas
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+     </BrowserRouter>
     </div>
   );
 }
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCuy0CveBiTlnudklSavyHgmgrzTQ1Qzc",
+  authDomain: "streaming-videos-e359e.firebaseapp.com",
+  projectId: "streaming-videos-e359e",
+  storageBucket: "streaming-videos-e359e.appspot.com",
+  messagingSenderId: "418074130063",
+  appId: "1:418074130063:web:7b0d9a547ecb2a9130a2fa",
+  measurementId: "G-LJTTFE9N4Q"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const dbFire = getFirestore(app)
+export { dbFire }
